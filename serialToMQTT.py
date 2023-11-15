4e350c29de8802f1c9acb3fe3f8ef1cbc42efc83
Andrés José Villalobos Bustos <44916243+Andres10976@users.noreply.github.com>
2023-11-15 12:53:21 -0600

    Variables globales simplificadas
    Se optimiza el código para capturar un par más de fallos y también se ordena más el código

--- serialToMQTT.py
+++ serialToMQTT.py
@@ -4,22 +4,27 @@ import json
 import paho.mqtt.client as mqtt
 from collections import OrderedDict
 
+#Global variables
+MQTT_username="Test1234"  #Ingresan su nombre de usuario asociados a MQTT
+MQTT_password="Test1234"  #Ingresan su contraseña asociados a MQTT
+MQTT_URL="c4e2607e9758412696b75708d95794bb.s1.eu.hivemq.cloud"   #Ingresa la URL asociado a su broker MQTT
+MQTT_port=8883  ##Ingresa el puerto asociado a su broker MQTT
+USB_port="COM3" #Cambiar por el puerto actual en uso del serial a USB
+ID_Cliente="ClientePrueba1234"  #Cambiar por ID actual del cliente
+ID_FACP="Edwards-IO1000-001"    #Cambiar por el ID actual del FACP
+
 # Initialize MQTT
 client = mqtt.Client()
-client.username_pw_set(username="", password="") #Ingresan su nombre de usuario y contraseña asociados a MQTT
+client.username_pw_set(username=MQTT_username, password=MQTT_password)
 client.tls_set()
-client.connect("URLBrokerMQTT",1883) #Ingresan la URL y el puerto asociado a su broker MQTT de la forma ("URL", puerto)
+client.connect(MQTT_URL,MQTT_port) 
 client.loop_start()
 
 # Event counter
 event_counter = 1
 
-from collections import OrderedDict
-
-from collections import OrderedDict
-
 def parse_serial_data(data):
-    metadata = {}
+    metadata = ""
     try:
         lines = list(filter(None, data.strip().split('\n')))
         if not lines:
@@ -36,10 +41,10 @@ def parse_serial_data(data):
 
         for meta in time_date_metadata[2:]:
             key, value = meta.split(':')
-            metadata[key.strip()] = value.strip()
+            metadata = metadata + key.strip()+ ": " + value.strip()
 
         if len(lines) > 1:
-            metadata['Additional_Metadata'] = lines[1].strip()
+            metadata = metadata + 'Additional_Metadata: ' + lines[1].strip()
 
     except Exception as e:
         print(f"An error occurred while parsing: {e}")
@@ -47,8 +52,8 @@ def parse_serial_data(data):
 
     # Create an ordered dictionary with fields in the desired order
     event_data = OrderedDict([
-        ("ID_Evento_Ocurrido", f"ClientePrueba1234-{event_counter:09d}"),
-        ("ID_Panel", "Edwards-IO1000-001"),
+        ("ID_Evento_Ocurrido", ID_Cliente+f"-{event_counter:09d}"),
+        ("ID_Panel", ID_FACP),
         ("ID_Evento", ID_Evento),
         ("Fecha", Fecha),
         ("Metadata", metadata)
@@ -61,22 +66,23 @@ def parse_serial_data(data):
 
 # Initialize serial port
 ser = serial.Serial()
-ser.port = 'COM3'
+ser.port = USB_port 
 ser.baudrate = 9600
 ser.bytesize = serial.EIGHTBITS
 ser.parity = serial.PARITY_NONE
 ser.stopbits = serial.STOPBITS_ONE
 ser.xonxoff = False
 
-ser.open()
-
-if ser.is_open:
-    print("Serial port is open.")
-else:
-    print("Failed to open serial port.")
+try:
+    ser.open()
+    if ser.is_open:
+        print("Serial port is open.")
+    else:
+        print("Failed to open serial port.")
+        exit(1)
+except:
+    print("Unexpected error ocurred opening the port.")
     exit(1)
-
-buffer = ""
 
 buffer = ""
 try:
@@ -91,10 +97,10 @@ try:
                 
                 if parsed_data is not None:
                     print(f"Parsed Data: {parsed_data}")  # Debugging statement
-                    parsed_data['ID_Evento_Ocurrido'] = f"ClientePrueba1234-{event_counter:09d}"
-                    parsed_data['ID_Panel'] = "Edwards-IO1000-001"
+                    parsed_data['ID_Evento_Ocurrido'] = ID_Cliente+f"-{event_counter:09d}"
+                    parsed_data['ID_Panel'] = ID_FACP
                     event_counter += 1
-                    client.publish("FACP/Eventos/ClientePrueba1234", json.dumps(parsed_data))
+                    client.publish("FACP/Eventos/"+ID_Cliente, json.dumps(parsed_data))
                 else:
                     print("Parsed data is None, skipping MQTT publish.")  # Debugging statement
                 
